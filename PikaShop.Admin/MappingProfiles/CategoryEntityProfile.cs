f6da135d19008c4fdbc9a5ff3d06ca5e42302f1f
HebaAliSayed <hebatAllah.a.sayed@gmail.com>
2024-03-21 06:57:13 +0200

    add category CRUD controller

---
+++ PikaShop.Admin/MappingProfiles/CategoryEntityProfile.cs
@@ -0,0 +1,15 @@
+﻿
+using AutoMapper;
+using PikaShop.Admin.ViewModels;
+using PikaShop.Data.Context.ContextEntities.Core;
+namespace PikaShop.Admin.MappingProfiles
+{
+    public class CategoryEntityProfile:Profile
+    {
+        public CategoryEntityProfile()
+        {
+            CreateMap<CategoryEntity, CategoryViewModel>().ReverseMap();
+            ShouldMapField = _ => false;
+        }
+    }
+}
---
+++ PikaShop.Admin/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PikaShop.Admin.ViewModels
+{
+    public class CategoryViewModel
+    {
+        [Key]
+        [Required]
+        [HiddenInput]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        public int? DepartmentID { get; set; }
+    }
+}
