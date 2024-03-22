54cc668045178fdfcfb9880639af1bffd98cd334
HebaAliSayed <hebatAllah.a.sayed@gmail.com>
2024-03-22 02:32:50 +0200

    add Product CRUD controller

---
+++ PikaShop.Admin/MappingProfiles/ProductEntityProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using PikaShop.Admin.ViewModels;
+using PikaShop.Data.Context.ContextEntities.Core;
+
+namespace PikaShop.Admin.MappingProfiles
+{
+    public class ProductEntityProfile:Profile
+    {
+        public ProductEntityProfile()
+        {
+            CreateMap<ProductEntity, ProductViewModel>().ReverseMap();
+            ShouldMapField = _ => false;
+        }
+    }
+}
---
+++ PikaShop.Admin/ViewModels/ProductViewModel.cs
@@ -0,0 +1,25 @@
+﻿#nullable disable
+
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PikaShop.Admin.ViewModels
+{
+    public class ProductViewModel
+    {
+        [Key]
+        [Required]
+        [HiddenInput]
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public int UnitsInStock { get; set; }
+
+        public string? Img { get; set; }
+        public int? CategoryID { get; set; }
+    }
+}
