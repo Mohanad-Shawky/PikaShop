f17cf722f8c5c5d775fb6dfaac90fe26eb92c5cd
Mohanad-Shawky <mohanadshaw2y@gmail.com>
2024-03-24 05:35:15 +0200

    Cleaned Layout with conditions on authorization

--- PikaShop.Admin/Controllers/HomeController.cs
+++ PikaShop.Admin/Controllers/HomeController.cs
@@ -1,27 +1,37 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using PikaShop.Admin.Models;
+using PikaShop.Data.Context.ContextEntities.Identity;
 
 namespace PikaShop.Admin.Controllers
 {
-    [Authorize(Roles = "SuperAdmin,Admin")]
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
+	[Authorize(Roles = "SuperAdmin,Admin")]
+	public class HomeController
+		(UserManager<ApplicationUserEntity> userManager,
+		SignInManager<ApplicationUserEntity> signInManager)
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		: Controller
+	{
+		readonly UserManager<ApplicationUserEntity> _userManager = userManager;
+		readonly SignInManager<ApplicationUserEntity> _signInManager = signInManager;
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		public async Task<IActionResult> Index()
+		{
+			var user = await _userManager.GetUserAsync(this.User);
+			return View(user);
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
